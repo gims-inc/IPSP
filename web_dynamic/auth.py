8aae5e883c178c75f7e3786b917bf1cb89452164
gims-inc <gabrielmusanga@gmail.com>
2023-02-23 16:45:17 +0300

    profile front

--- web_dynamic/auth.py
+++ web_dynamic/auth.py
@@ -1,16 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+import  uuid
 
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/register')
-def register():
-    return "<h1>register</h1>"
+# @auth.route('/register')
+# def register():
+#     return "<h1>register</h1>"
 
 
-@auth.route('/login')
+@auth.route('/login', strict_slashes=False)
 def login():
-    return "<h1>login</h1>"
+     return render_template('login.html',cache_id=uuid.uuid4())
 
 
 @auth.route('/logout')
--- web_dynamic/views.py
+++ web_dynamic/views.py
@@ -6,3 +6,14 @@ views = Blueprint('views',__name__)
 @views.route('/home',strict_slashes=False)
 def home():
     return render_template('home.html',cache_id=uuid.uuid4())
+
+
+@views.route('/profile_view',strict_slashes=False)
+def profile_view():
+    return render_template('view_profile.html',cache_id=uuid.uuid4())
+
+
+@views.route('/profile_user',strict_slashes=False)
+def profile_user():
+    return render_template('user_profile.html',cache_id=uuid.uuid4())
+
